5c454ead249d62422997a119940ee6d2ba85d3ad
Mayank <cs16btech11022@iith.ac.in>
2018-03-02 16:22:17 +0530

    Further integration added

---
+++ include/Platform.h
@@ -0,0 +1,14 @@
+//
+// Created by Mayank on 3/2/18.
+//
+
+#ifndef FIRENICE_PLATFORM_H
+#define FIRENICE_PLATFORM_H
+
+
+class Platform {
+
+};
+
+
+#endif //FIRENICE_PLATFORM_H
---
+++ src/Platform.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mayank on 3/2/18.
+//
+
+#include "Platform.h"
