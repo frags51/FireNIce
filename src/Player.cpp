41e0ae1d2e328df6564f500e566a8a1dbe327e59
Mayank <cs16btech11022@iith.ac.in>
2018-03-03 12:44:35 +0530

    Jump to be smoothened

--- src/Player.cpp
+++ src/Player.cpp
@@ -33,13 +33,14 @@ void Player::Update(float elapsedTime,sf::Event& _event,std::map<std::string, Vi
 {
     bool isCollide = false;
     for(auto it:_object){
+
         if(it.first!="Fireboy" && it.first!="Watergirl"){
             if(checkCollision(it.second,0.0f)){
                 isCollide = true;
             }
         }
     }
-    //if(isCollide) return ;
+    if(isCollide) return ;
 
     int row =0;
     bool toRight = true;
