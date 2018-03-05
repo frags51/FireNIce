6a60a84759129af463d88ebdeb483439ca5ad00a
Mayank <cs16btech11022@iith.ac.in>
2018-03-06 01:21:24 +0530

    Fire under

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -8,8 +8,8 @@
 GameState::state GameState::_state = Not_init; // Need to initialize these
 sf::RenderWindow GameState::_mainWindow;
 
-unsigned short GameState::port1 {45018};
-unsigned short GameState::port2 {45010};
+unsigned short GameState::port1 {49198};
+unsigned short GameState::port2 {49140};
 
 
 
