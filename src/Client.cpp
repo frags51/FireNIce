af68b43adaadd4b5229d0a8f4a7b0c1ce68be119
Supreet Singh <supreet51.cs@gmail.com>
2018-03-07 00:05:15 +0530

    Rm client code & document client

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,14 +1,3 @@
 #include <iostream>
 #include "Client.h"
 
-Client::Client() {}
-
-void Client::receive() {
-    recdStatus = std::async(&Client::rdr, this);
-}
-
-sf::Socket::Status Client::rdr() {
-    return this->listenSocket.receive(recd);
-};
-
-
