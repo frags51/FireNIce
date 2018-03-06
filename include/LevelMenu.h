712b7abf9f175e171bede90b5bdbf64d7967f377
AishwaryaGM <cs16btech11013@iith.ac.in>
2018-03-06 23:05:06 +0530

    commented LevelMenu.h

--- include/LevelMenu.h
+++ include/LevelMenu.h
@@ -6,19 +6,28 @@
 #include <SFML/Graphics.hpp>
 //#include <list>
 
+/**
+    @author G M Aishwarya
+    @date March 2018
+    */
+
 class LevelMenu{
+	/** Default constructor. Creates a new window to display the selected level.
+	    */
 public:
-    static const short N_LEVELS = 2;
-    LevelMenu(float width, float height);
+    static const short N_LEVELS = 2;/**Stores the number of levels*/
+    LevelMenu(float width, float height);/**controls the behavior of the generated window*/
 
     unsigned short show(sf::RenderWindow &renderWindow); // Returns the id of the pressed button
-    void up();
-    void down();
+    void up();/**increments the curIndex value i.e,level and also updates the text on the window*/
+    void down();/**decrements the curIndex value i.e,level and also updates the text on the window*/
 
 
 private:
-    unsigned short curIndex;
-    sf::Text item;
-    sf::Font font;
+    unsigned short curIndex;/**<initial curIndex i.e, level number*/
+    sf::Text item;/**<initial text on the screen*/
+    /**The text displayed on the generated window*/
+    sf::Font font;/**<initial font*/
+    /**The font of the text on the generated window*/
 };
 #endif //FIRENICE_LevelMenu_H
