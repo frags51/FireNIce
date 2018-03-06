f19bdd0889f6aa3eb0824cabc90bf14683bd9cca
AishwaryaGM <cs16btech11013@iith.ac.in>
2018-03-07 00:30:23 +0530

    commenbted LevelMenu.h and MainMenu.h

--- include/LevelMenu.h
+++ include/LevelMenu.h
@@ -1,4 +1,4 @@
-//! \brief This is a main menu object.
+//! \brief This is a level menu object.
 #ifndef FIRENICE_LEVELMENU_H
 #define FIRENICE_LEVELMENU_H
 
@@ -12,22 +12,28 @@
     */
 
 class LevelMenu{
-	/** Default constructor. Creates a new window to display the selected level.
+	/** Default constructor. new window for level selection.
 	    */
 public:
-    static const short N_LEVELS = 2;/**Stores the number of levels*/
-    LevelMenu(float width, float height);/**controls the behavior of the generated window*/
+    /**number of levels*/
+    static const short N_LEVELS = 2;
+    /**behavior of the LevelMenu window*/
+    LevelMenu(float width, float height);
 
     unsigned short show(sf::RenderWindow &renderWindow); // Returns the id of the pressed button
-    void up();/**increments the curIndex value i.e,level and also updates the text on the window*/
-    void down();/**decrements the curIndex value i.e,level and also updates the text on the window*/
+    /**increments the curIndex value (level number),updates the text on the window*/
+    void up();
+    /**decrements the curIndex value (level number),updates the text on the window*/
+    void down();
 
 
 private:
-    unsigned short curIndex;/**<initial curIndex i.e, level number*/
-    sf::Text item;/**<initial text on the screen*/
-    /**The text displayed on the generated window*/
-    sf::Font font;/**<initial font*/
-    /**The font of the text on the generated window*/
+    /**initial curIndex (level number)*/
+    unsigned short curIndex;
+    /**The text displayed on the LevelMenu window*/
+    sf::Text item;
+    /**The font of the text on the LevelMenu window*/
+    sf::Font font;
+    
 };
 #endif //FIRENICE_LevelMenu_H
